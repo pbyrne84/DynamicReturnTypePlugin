ffa47d8a2233dfc22703d6cbfc04c7de4fdca1c9
Patrick Bryne <p.bryne@sthree.com>
2013-05-23 20:49:45 +0100

    have it validating field references

--- src/com/ptby/dynamicreturntypeplugin/MethodCallValidator.java
+++ src/com/ptby/dynamicreturntypeplugin/MethodCallValidator.java
@@ -1,10 +1,13 @@
 package com.ptby.dynamicreturntypeplugin;
 
 import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.psi.elements.Field;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.PhpExpression;
+import com.jetbrains.php.lang.psi.elements.Variable;
 import com.jetbrains.php.lang.psi.elements.impl.FieldReferenceImpl;
 import com.jetbrains.php.lang.psi.elements.impl.MethodReferenceImpl;
+import com.jetbrains.php.lang.psi.elements.impl.PhpClassImpl;
 import com.jetbrains.php.lang.psi.elements.impl.VariableImpl;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 
@@ -20,6 +23,9 @@ public class MethodCallValidator {
         if ( !isCorrectMethodName ) {
             return false;
         }
+
+        System.out.println("");
+        System.out.println(classMethodConfig);
 
         PhpExpression classReference = methodReference.getClassReference();
         if ( classReference instanceof VariableImpl ) {
@@ -57,6 +63,35 @@ public class MethodCallValidator {
     }
 
     private boolean validateAgainstFieldReference( MethodReferenceImpl methodReference, ClassMethodConfig classMethodConfig, FieldReferenceImpl fieldReference ) {
+        System.out.println( "??? getReference " + methodReference.getReference() );
+        System.out.println( "??? getElement " + methodReference.getReference().getElement() );
+        String rawReference = fieldReference.getType().toString();
+        String[] split = rawReference.substring( 0, rawReference.lastIndexOf( '|') ).split( "(#P#C|\\.)" );
+
+
+        System.out.println( "??? getType" + split.length );
+        if (split.length < 3  ) {
+            return false;
+        }
+
+        String currentClassName = split[1];
+        String currentField  = split[2];
+
+        System.out.println("currentClassName "+ currentClassName);
+        System.out.println( "currentField "+ currentField );
+
+        PhpIndex phpIndex = PhpIndex.getInstance( methodReference.getProject() );
+        Collection<PhpClass> classesByFQN = phpIndex.getClassesByFQN( currentClassName );
+        for ( PhpClass phpClass : classesByFQN ) {
+            Collection<Field> fields = phpClass.getFields();
+            for ( Field field : fields ) {
+                if ( field.getName().equals( currentField ) && field.getType().toString().equals( classMethodConfig.getFqnClassName() ) ) {
+                    return true;
+                }
+            }
+
+        }
+
         return false;
     }
 }
