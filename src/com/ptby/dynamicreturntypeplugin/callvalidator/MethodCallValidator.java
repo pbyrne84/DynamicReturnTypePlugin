5c4d098c10158ec56102d7fa65c27e338733f7af
Patrick Bryne <p.bryne@sthree.com>
2013-06-19 19:09:04 +0100

    make final field validation and lookups happen in the get by signature lookup

--- src/com/ptby/dynamicreturntypeplugin/callvalidator/MethodCallValidator.java
+++ src/com/ptby/dynamicreturntypeplugin/callvalidator/MethodCallValidator.java
@@ -115,37 +115,6 @@ public class MethodCallValidator implements JsonConfigurationChangeListener {
             return false;
         }
 
-/*
-        String currentClassName = split[ 1 ];
-        String currentField = split[ 2 ];
-
-        Project project = methodReference.getProject();
-        PhpIndex phpIndex = PhpIndex.getInstance( project );
-
-        for( int i = 0; i < 100; i++  ){
-            System.out.println("fieldReference.getDeclaredType( " + i + ") " + fieldReference.getDeclaredType( i ) );
-
-        }
-        Collection <? extends PhpNamedElement> bySignature = phpIndex.getBySignature( "#P#C\\AdvertsOfDirectEmployersView.oTaskData", null, 0 );
-        System.out.println("bySignature.size() " + " " + rawReference + " " + bySignature.size() );
-
-        for ( PhpNamedElement phpNamedElement : bySignature ) {
-            System.out.println( "aa " + phpNamedElement.getName());
-            System.out.println( "aa " + phpNamedElement.getType());
-        }
-
-
-        Collection < PhpClass > classesByFQN = phpIndex.getClassesByFQN( currentClassName );
-        for ( PhpClass phpClass : classesByFQN ) {
-            Collection<Field> fields = phpClass.getFields();
-            for ( Field field : fields ) {
-                if ( field.getName().equals( currentField ) &&
-                        field.getType().toString().equals( classMethodConfig.getFqnClassName() ) ) {
-                    return true;
-                }
-            }
-        }*/
-
         return true;
     }
 
