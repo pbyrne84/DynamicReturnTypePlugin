db11b7370613802a5ee7a87bab4550d3e3015d8b
pbyrne84 <pbyrne84@gmail.com>
2014-02-27 21:23:12 +0000

    running the read action outside of a thread seems to be problematic

--- src/com/ptby/dynamicreturntypeplugin/file/FilenameSearcher.java
+++ src/com/ptby/dynamicreturntypeplugin/file/FilenameSearcher.java
@@ -9,29 +9,44 @@ import com.intellij.psi.search.ProjectAndLibrariesScope;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FilenameSearcher {
 
     public void findByFileName( final Project project, final String filename, final FilenameSearchResultsListener resultsListener ) {
-        final Collection<VirtualFile> files = ApplicationManager.getApplication().runReadAction(
-                new Computable<Collection<VirtualFile>>() {
-                    @Nullable
-                    @Override
-                    public Collection<VirtualFile> compute() {
-                        if ( project.isDisposed() ) {
-                            return null;
-                        }
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-                        Collection<VirtualFile> virtualFilesByName = FilenameIndex.getVirtualFilesByName(
-                                project,
-                                filename,
-                                new ProjectAndLibrariesScope( project, true )
+        executorService.execute(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        final Collection<VirtualFile> files = ApplicationManager.getApplication().runReadAction(
+                                new Computable<Collection<VirtualFile>>() {
+                                    @Nullable
+                                    @Override
+                                    public Collection<VirtualFile> compute() {
+                                        if ( project.isDisposed() ) {
+                                            return null;
+                                        }
+
+                                        Collection<VirtualFile> virtualFilesByName = FilenameIndex
+                                                .getVirtualFilesByName(
+                                                        project,
+                                                        filename,
+                                                        new ProjectAndLibrariesScope( project, true )
+                                                );
+                                        return virtualFilesByName;
+                                    }
+                                }
                         );
-                        return virtualFilesByName;
+
+                        System.out.println( files.size() );
+                        resultsListener.respondToResults( files );
                     }
                 }
         );
+        executorService.shutdown();
 
-        resultsListener.respondToResults( files );
     }
 }
