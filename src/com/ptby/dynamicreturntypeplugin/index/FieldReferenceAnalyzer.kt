986c967f31ac722f7094d71e0062bd521c722105
PByrne84 <pbyrne84@gmail.com>
2015-02-06 18:11:31 +0000

    all parameters gets passed along in the signature

--- src/com/ptby/dynamicreturntypeplugin/index/FieldReferenceAnalyzer.kt
+++ src/com/ptby/dynamicreturntypeplugin/index/FieldReferenceAnalyzer.kt
@@ -92,7 +92,7 @@ public class FieldReferenceAnalyzer(private val configAnalyser: ConfigAnalyser) 
         public fun packageForGetTypeResponse(intellijReference: String?,
                                              methodName: String?,
                                              returnType: String?): String? {
-            return intellijReference + ":" + methodName + DynamicReturnTypeProvider.PARAMETER_SEPARATOR + returnType
+            return intellijReference + ":" + methodName + DynamicReturnTypeProvider.PARAMETER_START_SEPARATOR + returnType
         }
     }
 }
