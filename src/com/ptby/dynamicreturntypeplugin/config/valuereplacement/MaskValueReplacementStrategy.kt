bc6db0ed0c2699bf8e4f8f10230ea047f1601ba0
pbyrne84 <pbyrne84@gmail.com>
2015-09-08 19:45:36 +0100

    fix masks having issues with ::class

---
+++ src/com/ptby/dynamicreturntypeplugin/config/valuereplacement/MaskValueReplacementStrategy.kt
@@ -0,0 +1,39 @@
+package com.ptby.dynamicreturntypeplugin.config.valuereplacement
+
+import com.ptby.dynamicreturntypeplugin.signature_extension.isPhpClassConstantSignature
+import com.ptby.dynamicreturntypeplugin.signature_extension.stripPhpClassConstantReference
+
+public class MaskValueReplacementStrategy(private val mask: String) : ValueReplacementStrategy {
+
+
+    override fun toString(): String {
+        return "MaskValueReplacementStrategy{" + "\nmask='" + mask + '\'' + '}'
+    }
+
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) {
+            return true
+        }
+        if (o !is MaskValueReplacementStrategy) {
+            return false
+        }
+
+        return mask == o.mask
+    }
+
+
+    override fun hashCode(): Int {
+        return mask.hashCode()
+    }
+
+
+    override fun replaceCalculatedValue(currentValue: String): String {
+        val replacableValue = if ( currentValue.isPhpClassConstantSignature() ) {
+            currentValue.stripPhpClassConstantReference()
+        } else {
+            currentValue
+        }
+        return mask.format(replacableValue)
+    }
+}
